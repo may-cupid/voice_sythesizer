a468739aeb443d280e817d51a3758dfe43ab0c22
maye <may.cupid.07@gmail.com>
2026-04-01 13:18:39 +0200

    fixed the thing

--- test.py
+++ test.py
@@ -2,30 +2,21 @@ import numpy as np
 import sounddevice as sd
 
 def main():
-    mysound = white_noise()
+    mysound = sine_tone()
     sd.play(mysound)
     sd.wait()
 
-def sine_tone(
-        frequency: int=440
+def sine_tone(  
+        frequency: int=440,
         duration: float=1.0,
         amplitude: float=0.5,
         sample_rate: int=44100
 ) -> np.ndarray:
+        n_samples = int(sample_rate * duration)
+        time_points = np.linspace(0, duration, n_samples, False)
+        sine = np.sin(2* np.pi * frequency * time_points)
+        sine *= amplitude
+        return sine
+print(sine_tone)
 
-"""generate sine tone"""
-
-n_samples = int(sample_rate * duration)
-timepoints = np.linspace(0, duration, n_samples, False)
-sine = np.sin(2* np.pi * frequency * time_points)
-sine *= amplitude
-return sine
-
-
-def test_noise(
-        duration: float=1.0,
-        amplitude: float=0.5,
-        sample_rate: int=44100
-) -> np.ndarray:
-    
-
+main()
